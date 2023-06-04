cc7554a18442bd546e8836ca40ea139a2e4fdf5e
pku-JamesLuo <131878940+pku-JamesLuo@users.noreply.github.com>
2023-06-04 10:40:14 +0800

    Add files via upload

---
+++ MiniMaze_V2/main.cpp
@@ -0,0 +1,11 @@
+#include "minimazegamewidgetiii.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MiniMazeGameWidgetIII w;
+    w.show();
+
+    return a.exec();
+}
